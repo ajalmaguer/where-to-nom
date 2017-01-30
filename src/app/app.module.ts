33754fb1e8370e6cdc3559e1e4dc1d51b54639d1
AJ Almaguer <ajalmaguer@gmail.com>
2017-01-29 21:52:25 -0800

    Revert "setup bootstrap navbar with routes"
    This reverts commit 8d5f3e206a8957fd9444997479d625ce90a3f69b.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app.routes'
 import { AppComponent } from './app.component';
-import { LocalStorageService } from './local-storage.service';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PlayComponent } from './play/play.component';
-import { HostComponent } from './host/host.component';
-import { HomeComponent } from './home/home.component';
+import { LocalStorageService } from './local-storage.service'
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		NavbarComponent,
-		PlayComponent,
-		HostComponent,
-		HomeComponent,
 	],
 	imports: [
-		AppRoutingModule,
 		BrowserModule,
 		FormsModule,
 		HttpModule
